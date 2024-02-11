7d58089fc626294db56752d4202363296e22515a
Pascal Chatonnay <pascal.chatonnay@univ-fcomte.fr>
2024-02-11 16:37:45 +0100

    Configuration initiale des fichiers
    mise en place

---
+++ script.js
@@ -0,0 +1,13 @@
+// Author: Pascal Chatonnay
+// Date: 2019/11/20
+// Description: les script pour un algorithme génétique simple
+
+// Variables globales
+var population = [];
+var populationSize = 10;
+var mutationRate = 0.01;
+var target = "Hello, World!";
+var generation = 0;
+
+
+
