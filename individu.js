a0543bad8dc65c857c8264fa4dad245bec24aee8
Pascal Chatonnay <pascal.chatonnay@univ-fcomte.fr>
2024-02-12 07:44:19 +0100

    creation des individus
    initialisation de l'objet, creation, evaluation basique

---
+++ individu.js
@@ -0,0 +1,38 @@
+// définition des individus
+
+// taille d'un individu
+var individuSize = 10;
+
+// définition du génotype
+function randomGenotype() {
+    var genotype = [];
+    for (var i = 0; i < individuSize; i++) {
+        genotype[i] = Math.round(Math.random());
+    }
+    return genotype;
+}
+
+// function decodeGenotype : covertie en entier un génotype
+function decodeGenotype(genotype) {
+    var x = 0;
+    for (var i = 0; i < individuSize; i++) {
+        x += genotype[i] * Math.pow(2, i);
+    }
+    return x;
+}
+
+// fonction fitness : fonction à optimiser
+function fitness(x) {
+    return x * x;
+}
+
+
+// defini un individu avec un génotype et un phénotype
+function Individu() {
+    this.genotype = [];
+    this.phenotype = [];
+    this.fitness = 0;
+    this.genotype = randomGenotype();
+    this.phenotype = decodeGenotype(this.genotype);
+    this.fitness = fitness(this.phenotype);
+}
